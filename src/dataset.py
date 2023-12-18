41a6b5c4c970e58bcae30c8c905750d3af56f9ff
tqtensor <victortang212@gmail.com>
2023-12-18 00:38:05 +0000

    feat: add multiprocessing for features generator

--- src/dataset.py
+++ src/dataset.py
@@ -1,4 +1,6 @@
 import glob
+import multiprocessing
+import multiprocessing as mp
 import os
 import shutil
 import zipfile
@@ -8,10 +10,11 @@ import librosa
 import numpy as np
 import pandas as pd
 import parselmouth
-from acoustic_features import extract_feature_means
 from praatio.utilities import textgrid_io
 from tqdm.auto import tqdm
 from unidecode import unidecode
+
+from acoustic_features import extract_feature_means
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,21 +25,21 @@ if not os.path.exists("bahnaric.zip"):
         "bahnaric.zip",
     )
 
+if os.path.exists("bahnaric"):
+    shutil.rmtree("bahnaric")
+
 # Unzip the ZIP file
 with zipfile.ZipFile("bahnaric.zip", "r") as zip_file:
     zip_file.extractall()
-if os.path.exists("bahnaric/dataset/Bana_1321_1650"):
-    shutil.rmtree("bahnaric/dataset/Bana_1321_1650")
-shutil.move("Bana_1321_1650", "bahnaric/dataset")
+shutil.move("Bana_1321_1650", "bahnaric/dataset/raw")
+os.remove("bahnaric.zip")
 
 # --------------------------------------------------------------------------- #
 # Parse TextGrid
 
-if not os.path.exists("bahnaric/dataset/features"):
-    os.makedirs("bahnaric/dataset/features")
 
 labels = []
-for textgrid_file in glob.glob("bahnaric/dataset/*/*.TextGrid"):
+for textgrid_file in glob.glob("bahnaric/dataset/*/*/*.TextGrid"):
     textgrid_data = parselmouth.Data.read(textgrid_file)
     textgrid_data.save_as_text_file("/tmp/textgrid.txt")
 
@@ -75,9 +78,50 @@ for textgrid_file in glob.glob("bahnaric/dataset/*/*.TextGrid"):
 labels = pd.DataFrame(labels)
 labels.to_csv("bahnaric/labels.csv", index=False)
 
-
 # --------------------------------------------------------------------------- #
 # Generate acoustic features
+
+
+def _par_features_generator(file_name: str):
+    signal, sr = librosa.load(file_name.replace("TextGrid", "wav"), sr=16000)
+
+    feature_dfs = []
+    for k in range(5):
+        # Define window size
+        k = 75 + (1 + k) * 10
+        assert k % 2 == 1, "k must be odd"
+
+        # Break audio into frames
+        frame_length = int(sr * 0.005)  # 5ms
+        hop_length = int(sr * 0.001)  # 1ms
+        frames = librosa.util.frame(
+            signal, frame_length=frame_length, hop_length=hop_length
+        )
+
+        # Pad frames at the beginning and end
+        padding = (k - 1) // 2
+        padded_frames = np.pad(frames, ((0, 0), (padding, padding)), mode="edge")
+
+        # Calculate features on sliding window of k frames
+        features = []
+        for i in range(padding, len(padded_frames[0]) - padding):
+            window = padded_frames[:, i - padding : i + padding + 1]
+            feature = extract_feature_means(signal=window.flatten(), sr=sr)
+            features.append(feature)
+
+        features = pd.concat(features, axis=0)
+        features.columns = [f"{col}_w{str(k).zfill(3)}" for col in features.columns]
+        feature_dfs.append(features)
+
+    features = pd.concat(feature_dfs, axis=1)
+    1 / 0
+    features.to_parquet(
+        os.path.join(
+            "bahnaric/features",
+            file_name.split("/")[-1].replace("TextGrid", "parquet"),
+        )
+    )
+
 
 if not os.path.exists("bahnaric/features"):
     os.makedirs("bahnaric/features")
@@ -99,48 +143,16 @@ file_names = [
     if os.path.exists(file_name.replace("TextGrid", "wav"))
 ]
 
-for file_name in tqdm(file_names, total=len(file_names)):
-    signal, sr = librosa.load(file_name.replace("TextGrid", "wav"), sr=16000)
+# Run _par_features_generator in parallel
+num_cpus = mp.cpu_count() - 2
+pool = multiprocessing.Pool(num_cpus)
 
-    feature_dfs = []
-    for k in range(5):
-        # Define window size
-        k = 75 + (1 + k) * 10
-        assert k % 2 == 1, "k must be odd"
+# Use tqdm to show progress
+with tqdm(total=len(file_names)) as pbar:
 
-        # Break audio into frames
-        frame_length = int(sr * 0.005)  # 5ms
-        hop_length = int(sr * 0.001)  # 1ms
-        frames = librosa.util.frame(
-            signal, frame_length=frame_length, hop_length=hop_length
-        )
+    def update_progress(*args):
+        pbar.update()
 
-        # Calculate the timestamp of each frame
-        timestamps = np.arange(frames.shape[1]) * hop_length / sr
-
-        # Pad frames at the beginning and end
-        padding = (k - 1) // 2
-        padded_frames = np.pad(
-            frames, ((0, 0), (padding, padding)), mode="edge"
-        )
-
-        # Calculate features on sliding window of k frames
-        features = []
-        for i in range(padding, len(padded_frames[0]) - padding):
-            window = padded_frames[:, i - padding : i + padding + 1]
-            feature = extract_feature_means(signal=window.flatten(), sr=sr)
-            features.append(feature)
-
-        features = pd.concat(features, axis=0)
-        features.columns = [
-            f"{col}_w{str(k).zfill(3)}" for col in features.columns
-        ]
-        feature_dfs.append(features)
-
-    features = pd.concat(feature_dfs, axis=1)
-    features.to_parquet(
-        os.path.join(
-            "bahnaric/features",
-            file_name.split("/")[-1].replace("TextGrid", "parquet"),
-        )
-    )
+    pool.map_async(_par_features_generator, file_names, callback=update_progress)
+    pool.close()
+    pool.join()
